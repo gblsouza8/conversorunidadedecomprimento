23a3e1ffe533272751c20a7e94f80a1e48a6c4b3
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-11 16:29:37 -0300

    Segundo Commit

---
+++ conversor.py
@@ -0,0 +1,10 @@
+def conversor(metros):
+    quilometro = metros/1000
+    centimetro = metros*100
+    milimetro = metros*1000
+    milha = metros/1609
+    jarda = metros*1.094
+    pes = metros*3.281
+    polegada = metros*39.37
+    milhaNautica = metros/1852
+    
